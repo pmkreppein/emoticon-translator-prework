2cb3e5c896ebc89c2519776161e3bc487a9ca23f
Peter Kreppein <pmkreppein@gmail.com>
2018-09-29 00:09:34 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,13 +2,13 @@ require "yaml"
 
 def load_library(file)
   lib = 
-  {:get_meaning => {}, 
-  :get_emoticon => {} }
+  {"get_meaning" => {}, 
+  ""get_emoticon"" => {} }
   YAML.load_file(file).each do |trans, array|
     eng = Array.new
     jpn = Array.new
-    lib[:get_emoticon][eng] = jpn
-    lib[:get_meaning][jpn] = trans
+    lib["get_emoticon"][eng] = jpn
+    lib["get_meaning"][jpn] = trans
   end
   lib
 end
